bd807575f063ec18f660691e6e05bfa075165907
Michael Mitrakos <mike.mitrakos@gmail.com>
2017-01-27 15:42:10 -0800

    [update] - update image urls

--- src/app/home/action/action.component.ts
+++ src/app/home/action/action.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ActionComponent implements OnInit {
 
   tiles = [
-      {text: 'Eyesight for Addie', description: '', img: '../assets/images/VisionforAddie.jpg', cols: 2, rows: 2, color: 'lightblue'},
+      {text: 'Eyesight for Addie', description: '', img: 'https://s27.postimg.org/a1v0rvxcv/Visionfor_Addie.jpg', cols: 2, rows: 2, color: 'lightblue'},
       {text: 'Two', description: '', cols: 2, rows: 2, color: 'lightgreen'},
       {text: 'Three', description: '', cols: 2, rows: 2, color: 'purple'}
     ];
--- src/app/home/what-we-do/what-we-do.component.ts
+++ src/app/home/what-we-do/what-we-do.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class WhatWeDoComponent implements OnInit {
 
     tiles = [
-      {text: 'Civility', description: '', img: '../assets/images/VisionforAddie.jpg', cols: 2, rows: 2, color: 'lightblue'},
+      {text: 'Civility', description: '', img: 'https://s27.postimg.org/a1v0rvxcv/Visionfor_Addie.jpg', cols: 2, rows: 2, color: 'lightblue'},
       {text: 'Diversity', description: '', cols: 2, rows: 2, color: 'lightgreen'},
       {text: 'Tolerance', description: '', cols: 2, rows: 2, color: 'lightpink'},
       {text: 'Acceptance', description: '', cols: 2, rows: 2, color: '#DDBDF1'},
