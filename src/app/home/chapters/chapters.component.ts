5c73f490537860b194edadd3c9c3bd2cb9de5392
Michael Mitrakos <mike.mitrakos@gmail.com>
2017-01-27 20:51:26 -0800

    [update] - update image links for higher quality

--- src/app/home/chapters/chapters.component.ts
+++ src/app/home/chapters/chapters.component.ts
@@ -11,21 +11,21 @@ export class ChaptersComponent implements OnInit {
     {
       name: "Miami University Oxford",
       url: "https://www.facebook.com/MUProjectCivility",
-      imageUrl: "https://s27.postimg.org/p6mjbnw67/miami_University.jpg",
+      imageUrl: "https://s27.postimg.org/6r22e9i1v/miami_University.jpg",
       members: 15,
       adminName: "Shar Qureshi"
     },
     {
       name: "Miami University Hamilton",
       url: "https://www.facebook.com/MUProjectCivility",
-      imageUrl: "https://s27.postimg.org/8sxyerqn3/miami_Hamilton.jpg",
+      imageUrl: "https://s27.postimg.org/t0be72o4j/miami_Hamilton.jpg",
       members: 22,
       adminName: "Shan Qureshi"
     },
     {
       name: "Miami University Middletown",
       url: "https://www.facebook.com/MUProjectCivility",
-      imageUrl: "https://s27.postimg.org/e5msswejj/miami_Middletown.jpg",
+      imageUrl: "https://s27.postimg.org/96zaedaqr/miami_Middletown.jpg",
       members: 18,
       adminName: "Sandra Bullock"
     }
