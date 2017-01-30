c5571167ab6709836cdc59022e1374ec1e4ee222
Michael Mitrakos <mike.mitrakos@gmail.com>
2017-01-29 22:14:57 -0800

    [minor changes] - minor changes to styling and teams view

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 // server.js
 const express = require('express');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
+
 app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
+
 app.listen(process.env.PORT || 8080, () => {
     console.log("Server started")
 });
@@ -20,7 +18,5 @@ const forceSSL = function() {
     next();
   }
 }
-// Instruct the app
-// to use the forceSSL
-// middleware
+
 app.use(forceSSL());
