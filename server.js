3f816428191c626d60b085080583b806a78317ef
Michael Mitrakos <mike.mitrakos@gmail.com>
2017-01-31 23:28:16 -0800

    [remove] - remove SSL

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ app.listen(process.env.PORT || 8080, () => {
     console.log("Server started")
 });
 
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//        ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// }
 
-app.use(forceSSL());
+// app.use(forceSSL());
