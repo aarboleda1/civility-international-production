1c4808ab9adfa871197a73c0f81a7602cd7408e3
Michael Mitrakos <mike.mitrakos@gmail.com>
2017-02-01 07:54:48 -0800

    [remove] - remove chapters

--- src/app/home/chapters/chapters.component.ts
+++ src/app/home/chapters/chapters.component.ts
@@ -8,27 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class ChaptersComponent implements OnInit {
 
   public chapters = [
-    {
-      name: "Miami University Oxford",
-      url: "https://www.facebook.com/MUProjectCivility",
-      imageUrl: "https://s27.postimg.org/6r22e9i1v/miami_University.jpg",
-      members: 15,
-      adminName: "Shar Qureshi"
-    },
-    {
-      name: "Miami University Hamilton",
-      url: "https://www.facebook.com/MUProjectCivility",
-      imageUrl: "https://s27.postimg.org/t0be72o4j/miami_Hamilton.jpg",
-      members: 22,
-      adminName: "Shan Qureshi"
-    },
-    {
-      name: "Miami University Middletown",
-      url: "https://www.facebook.com/MUProjectCivility",
-      imageUrl: "https://s27.postimg.org/96zaedaqr/miami_Middletown.jpg",
-      members: 18,
-      adminName: "Sandra Bullock"
-    }
+    // {
+    //   name: "Miami University Oxford",
+    //   url: "https://www.facebook.com/MUProjectCivility",
+    //   imageUrl: "https://s27.postimg.org/6r22e9i1v/miami_University.jpg",
+    //   members: 15,
+    //   adminName: "Shar Qureshi"
+    // },
+    // {
+    //   name: "Miami University Hamilton",
+    //   url: "https://www.facebook.com/MUProjectCivility",
+    //   imageUrl: "https://s27.postimg.org/t0be72o4j/miami_Hamilton.jpg",
+    //   members: 22,
+    //   adminName: "Shan Qureshi"
+    // },
+    // {
+    //   name: "Miami University Middletown",
+    //   url: "https://www.facebook.com/MUProjectCivility",
+    //   imageUrl: "https://s27.postimg.org/96zaedaqr/miami_Middletown.jpg",
+    //   members: 18,
+    //   adminName: "Sandra Bullock"
+    // }
   ]
 
   constructor() { }
