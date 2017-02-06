519dc345291294770f34e7d685fd57cdf18f7b62
Michael Mitrakos <mike.mitrakos@gmail.com>
2017-02-05 18:12:55 -0800

    [update] - update photos

--- src/app/home/what-we-do/what-we-do.component.ts
+++ src/app/home/what-we-do/what-we-do.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class WhatWeDoComponent implements OnInit {
 
     tiles = [
-      {text: 'Civility', description: '', img: 'https://s27.postimg.org/a1v0rvxcv/Visionfor_Addie.jpg', cols: 2, rows: 2, color: 'lightblue'},
-      {text: 'Diversity', description: '', cols: 2, rows: 2, color: 'lightgreen'},
-      {text: 'Tolerance', description: '', cols: 2, rows: 2, color: 'lightpink'},
-      {text: 'Acceptance', description: '', cols: 2, rows: 2, color: '#DDBDF1'},
+      {text: 'http://i.imgur.com/TEGB0s4.jpg', description: '', img: 'https://s27.postimg.org/a1v0rvxcv/Visionfor_Addie.jpg', cols: 2, rows: 2, color: 'lightblue'},
+      {text: 'http://i.imgur.com/9lHVMHR.jpg', description: '', cols: 2, rows: 2, color: 'lightgreen'},
+      {text: 'http://i.imgur.com/E6TylNX.jpg', description: '', cols: 2, rows: 2, color: 'lightpink'},
+      {text: 'http://i.imgur.com/40xdQIa.jpg', description: '', cols: 2, rows: 2, color: '#DDBDF1'},
     ];
 
   constructor() { }
